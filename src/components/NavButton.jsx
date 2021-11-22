cc94eb1f4c6adecdfd351a9466ca491efd367a88
Matteo Miceli <omatteomiceli@gmail.com>
2021-11-21 19:56:00 -0800

    added navbar animations

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-export default function NavButton({ icon }) {
+export default function NavButton({ icon, navExtend }) {
     return (
         <a href={`#${icon.name}`}>
-            <div className="h-16 flex justify-center items-center hover:bg-gray-800 transition-all duration-200">
-                <img className="w-6" src={icon.url} alt={icon.name} />
+            <div className={`h-12 my-3 overflow-hidden flex ${navExtend? 'justify-start' : 'justify-center'} items-center hover:bg-gray-800 transition-all duration-200`}>
+                <img className={`w-6 ${navExtend? 'ml-2' : '' }`} src={icon.url} alt={icon.name} />
+                <p className={`${navExtend? 'ml-2' : 'max-w-0 opacity-0'} transition-all duration-300`}>{icon.name}</p>
             </div>
         </a>
     );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,36 @@
-import { Link } from "gatsby"
-import React from "react"
-import {navIcons} from "../content/navIcons"
-import NavButton from "./NavButton"
+import { Link } from "gatsby";
+import React, { useState } from "react";
+import { navIcons } from "../content/navIcons";
+import NavButton from "./NavButton";
 
 export default function Navbar() {
+    const [navExtend, setNavExtend] = useState(false);
+
+    function handleMouseEnter() {
+        setNavExtend(true);
+    }
+    function handleMouseLeave() {
+        setNavExtend(false);
+    }
+
     return (
-        <div className="fixed h-screen w-12 bg-black border-r-1 border-white">
-            <Link to="#home" className="flex justify-center items-center text-lg h-12 bg-dark-pink">
-                {'{'}<span className="name-slide max-w-0 opacity-0">matteo</span>{'}'}
+        <div
+            className={`fixed h-screen ${navExtend? 'w-32' : 'w-12'} bg-black border-r-1 border-white transition-all duration-200`}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        >
+            <Link
+                to="#home"
+                className="flex justify-center items-center text-lg h-12 bg-dark-pink overflow-hidden"
+            >
+                {"{"}
+                <span className={`name-slide ${navExtend? 'max-w-none opacity-100' : 'max-w-0 opacity-0 '} transition-all duration-200`}>matteo</span>
+                {"}"}
             </Link>
 
             {navIcons.map((icon) => {
-                return <NavButton icon={icon} />
+                return <NavButton icon={icon} navExtend={navExtend} />;
             })}
         </div>
-    )
+    );
 }
