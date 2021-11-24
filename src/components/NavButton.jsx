de6e327eebc36092dcace3a10bb1c1a39b74a32b
Matteo Miceli <omatteomiceli@gmail.com>
2021-11-23 23:14:38 -0800

    aligned icons in navbar for smoother transition

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function NavButton({ icon, navExtend }) {
     return (
         <a href={`#${icon.name}`}>
-            <div className={`h-12 my-3 overflow-hidden flex ${navExtend? 'justify-start' : 'justify-center'} items-center hover:bg-gray-800 transition-all duration-200`}>
-                <img className={`w-6 ${navExtend? 'ml-2' : '' }`} src={icon.url} alt={icon.name} />
+            <div className={`h-12 my-3 overflow-hidden flex ${navExtend? 'justify-start' : 'justify-start'} items-center hover:bg-gray-800 transition-all duration-200`}>
+                <img className={`w-6 ${navExtend? 'ml-3' : 'ml-3' }`} src={icon.url} alt={icon.name} />
                 <p className={`${navExtend? 'ml-2' : 'max-w-0 opacity-0'} transition-all duration-300`}>{icon.name}</p>
             </div>
         </a>
