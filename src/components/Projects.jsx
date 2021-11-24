c05c1a984e0169ccfa6226e5e589029275778309
matteomiceli <omatteomiceli@gmail.com>
2021-11-24 00:34:15 -0800

    projects page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,19 +3,27 @@ import projectList from "../content/projects";
 
 export default function Projects() {
     return (
-        <div className="h-screen w-full">
-            <h2 className="text-white text-3xl font-header font-bold">
+        <div className="h-screen w-full mt-28 ">
+            <h2
+                id="projects"
+                className="text-white text-3xl font-header font-bold"
+            >
                 {">"} Projects
             </h2>
-            {projectList.map((proj, i) => {
-                return (
-                <div className="flex flex-wrap mt-28 ">
-                    <div className="w-96">
-                        <h3 className="text-neon-blue text-xl">0{i}. {proj.name}</h3>
-                        <p>{proj.description}</p>
-                    </div>
-                </div>);
-            })}
+            <div className="mt-28 flex flex-wrap justify-around">
+                {projectList.map((proj, i) => {
+                    return (
+                        <a className="mx-1 my-8 w-2/6" href={`#project-${i}`}>
+                            <div className="border-1 hover:border-gray-300 p-4">
+                                <h3 className="text-neon-blue text-2xl">
+                                    0{i}. {proj.name}
+                                </h3>
+                                <p>{proj.description}</p>
+                            </div>
+                        </a>
+                    );
+                })}
+            </div>
         </div>
     );
 }
--- src/content/projects.js
+++ src/content/projects.js
@@ -6,7 +6,24 @@ const projectList = [
     {
         name: 'Int-house',
         description: 'Compete against your friends in custom in house e-sports leagues.'
+    },
+    {
+        name: 'TEDxSFU',
+        description: 'Website for the 2021 TEDxSFU conference.'
+    },
+    {
+        name: 'VXP',
+        description: "Website for Canada's first design-focused hackathon."
+    },
+    {
+        name: 'Craigslist Redesign',
+        description: 'Browse through a registry of items for sale and post your own ads.'
+    },
+    {
+        name: 'Nammos',
+        description: 'Website for a local restaurant.'
     }
+
 ]
 
 export default projectList;
