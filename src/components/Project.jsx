b07805e8afac976807fc61cf344f732dd7575934
Matteo Miceli <omatteomiceli@gmail.com>
2021-11-29 14:04:50 -0800

    for user testing

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,8 +4,47 @@ export default function Project({ project, i }) {
     return (
         <div className="pt-8 h-full" id={`project-${i}`}>
             <h2 className="text-neon-blue text-3xl">{`0${i}. ${project.name}`}</h2>
-            <div>
-                
+            <div className="flex">
+                <div className="mt-10 w-1/2 text-sm">
+                    <div className="absolute w-px bg-neon-blue h-3/6"></div>
+                    <p className="ml-4 mt-4">
+                        <span className="text-neon-blue">Role: </span>
+                        {project.role}
+                    </p>
+                    <p className="ml-4 mt-8">{project.longDescription}</p>
+                    <div className="ml-4 mt-24 buttons flex">
+                        <a href={project.links.website}>
+                            <button className="flex justify-center w-32 bg-dark-pink p-2 border-2 border-dark-pink hover:bg-dark-pink-hover hover:border-dark-pink-hover rounded transition-all duration-200">
+                                <img
+                                    className="w-4 mr-2"
+                                    src="/icons/web.svg"
+                                    alt="web icon"
+                                />
+                                Live Site
+                            </button>
+                        </a>
+                        <a href={project.links.github}>
+                            <button className="flex justify-center ml-8 w-32 border-2 border-white p-2 rounded hover:border-gray-300 hover:text-gray-300 transition-all duration-200">
+                                <img
+                                    className="w-4 mr-2"
+                                    src="/icons/github.svg"
+                                    alt={project.name}
+                                />
+                                Github
+                            </button>
+                        </a>
+                    </div>
+                </div>
+                <div className="flex flex-col w-1/2 items-center">
+                    <a className="mt-2 flex justify-center" href={project.links.website}>
+                        <img className="w-4/5 rounded-lg" src={project.imgPath} alt="" />
+                    </a>
+                    <div className="stack flex w-5/6 justify-around">
+                        {project.techStack.map((item) => {
+                            return <img className="mt-8 w-8" src={`/icons/${item}.svg`} alt="" />
+                        })}
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/content/projects.js
+++ src/content/projects.js
@@ -2,26 +2,81 @@ const projectList = [
     {
         name: 'Runik',
         description: 'Generate custom e-reader dictionaries for your favourite fictional worlds.',
+        role: 'Project Lead, Full Stack Web Developer',
+        longDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        links: {
+            website: 'https://runik.app',
+            github: 'https://github.com/Runik-3'
+        },
+        imgPath: '/images/runik.png',
+        techStack: [
+            'nextdotjs',
+            'tailwindcss',
+            'nodedotjs',
+            'express',
+            'flask',
+            'mongodb'
+        ]
     },
     {
         name: 'Int-house',
-        description: 'Compete against your friends in custom in house e-sports leagues.'
+        description: 'Compete against your friends in custom in house e-sports leagues.',
+        role: '',
+        longDescription: '',
+        links: {
+            website: '',
+            github: ''
+        },
+        imgPath: '',
+        techStack: []
     },
     {
         name: 'TEDxSFU',
-        description: 'Website for the 2021 TEDxSFU conference.'
+        description: 'Website for the 2021 TEDxSFU conference.',
+        role: '',
+        longDescription: '',
+        links: {
+            website: '',
+            github: ''
+        },
+        imgPath: '',
+        techStack: []
     },
     {
         name: 'VXP',
-        description: "Website for Canada's first design-focused hackathon."
+        description: "Website for Canada's first design-focused hackathon.",
+        role: '',
+        longDescription: '',
+        links: {
+            website: '',
+            github: ''
+        },
+        imgPath: '',
+        techStack: []
     },
     {
         name: 'Craigslist Redesign',
-        description: 'Browse through a registry of items for sale and post your own ads.'
+        description: 'Browse through a registry of items for sale and post your own ads.',
+        role: '',
+        longDescription: '',
+        links: {
+            website: '',
+            github: ''
+        },
+        imgPath: '',
+        techStack: []
     },
     {
         name: 'Nammos',
-        description: 'Website for a local restaurant.'
+        description: 'Website for a local restaurant.',
+        role: '',
+        longDescription: '',
+        links: {
+            website: '',
+            github: ''
+        },
+        imgPath: '',
+        techStack: []
     }
 
 ]
