61b3cd6e8b413278adf1474a8ae1ca4d26e19de6
matteomiceli <omatteomiceli@gmail.com>
2022-01-30 20:01:49 -0800

    color styling on project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,22 +3,22 @@ import React from "react";
 export default function Project({ project, i }) {
     return (
         <div className="pt-8 h-full" id={`project-${i}`}>
-            <h2 className="text-neon-blue text-3xl">{`0${i}. ${project.name}`}</h2>
+            <h2 className="text-purple text-3xl">{`0${i}. ${project.name}`}</h2>
             <div className="flex">
                 <div className="mt-10 w-1/2 pr-10">
-                    <div className="absolute w-px bg-neon-blue h-3/6"></div>
+                    <div className="absolute w-px bg-purple h-3/6"></div>
                     <p className="ml-4 mt-4">
-                        <span className="text-neon-blue">Type: </span>
+                        <span className="font-bold text-purple">Type: </span>
                         {project.type}
                     </p>
                     <p className="ml-4 mt-4">
-                        <span className="text-neon-blue">Role: </span>
+                        <span className="font-bold text-purple">Role: </span>
                         {project.role}
                     </p>
                     <p className="ml-4 mt-8">{project.longDescription}</p>
                     <div className="ml-4 mt-24 buttons flex">
                         <a target="_blank" href={project.links.website}>
-                            <button className="flex justify-center w-32 bg-dark-pink p-2 border-2 border-dark-pink hover:bg-dark-pink-hover hover:border-dark-pink-hover rounded transition-all duration-200">
+                            <button className="flex justify-center w-32 bg-gradient-to-br p-2 nav-home bg-scale4 from-dark-purple via-light-purple to-dark-pink rounded transition-all duration-300">
                                 <img
                                     className="w-4 mr-2"
                                     src="/icons/web.svg"
@@ -28,7 +28,7 @@ export default function Project({ project, i }) {
                             </button>
                         </a>
                         <a target="_blank" href={project.links.github}>
-                            <button className="flex justify-center ml-8 w-32 border-2 border-white p-2 rounded hover:border-gray-300 hover:text-gray-300 transition-all duration-200">
+                            <button className="flex justify-center ml-8 w-32 border-2 border-white p-2 rounded hover:border-gray-300 hover:text-gray-300 transition-all duration-300">
                                 <img
                                     className="w-4 mr-2"
                                     src="/icons/github.svg"
