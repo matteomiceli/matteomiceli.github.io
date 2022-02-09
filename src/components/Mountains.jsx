fb1253ec6bed40f7c77704707a2476e4491e4bbd
matteomiceli <omatteomiceli@gmail.com>
2022-02-09 03:06:28 -0800

    md mountains

--- src/components/Mountains.jsx
+++ src/components/Mountains.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Mountains() {
     return (
-        <div className="absolute mt-40">
+        <div className="mt-6 lg:-mt-40 xl:-mt-56 overflow-hidden">
             <img
-                className="w-screen"
+                className="overflow-hidden max-w-6xl lg:max-w-full -ml-44 lg:ml-0 translate-x-80 lg:w-screen"
                 src="/icons/mountains-dark.svg"
                 alt="neon mountains"
             />
