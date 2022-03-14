21f476a76ea74e9491203d90fa05b303343c1eb3
Matteo Miceli <matteo@pop-os.localdomain>
2022-03-14 15:16:54 -0700

    styled back to projects button

--- src/components/ProjectNavigation.jsx
+++ src/components/ProjectNavigation.jsx
@@ -6,7 +6,7 @@ export default function ProjectNavigation() {
     return (
         <div className="flex justify-center w-full sticky top-6 z-20 ">
             <Link to="/#projects">
-                <button className="flex bg-scale2 nav-home font-semibold rounded-md py-1 px-3 text-bg-dark bg-gradient-to-br from-teal via-blue to-dark-purple transition-all duration-300">
+                <button className="flex items-center bg-scale2 nav-home font-semibold rounded-full py-1 px-4 text-bg-dark bg-gradient-to-br from-teal via-blue to-dark-purple transition-all duration-300">
                     <p className="mr-1">back to projects</p> <Chevron />
                 </button>
             </Link>
--- src/components/icons/Chevron.jsx
+++ src/components/icons/Chevron.jsx
@@ -2,19 +2,19 @@ import * as React from "react";
 
 const Chevron = (props) => (
     <svg
-        width={24}
-        height={24}
+        width={12.006}
+        height={9.242}
         fill="none"
         stroke="currentColor"
         strokeWidth={2}
         strokeLinecap="round"
         strokeLinejoin="round"
-        className="feather pt-0.5 feather-chevron-up"
+        className="feather feather-chevron-up ml-0.5"
         xmlns="http://www.w3.org/2000/svg"
         {...props}
     >
         <path
-            transform="translate(-1.74 -2.116)"
+            transform="matrix(.85756 0 0 1.15528 -4.288 -9.242)"
             style={{
                 stroke: "#181818",
                 strokeOpacity: 1,
